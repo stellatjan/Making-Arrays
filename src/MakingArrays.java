c121199b998987a84e98e0b79d63365d511ade29
stellatjan <stella_tjan27@milton.edu>
2025-02-19 11:25:50 -0500

    inital

---
+++ src/MakingArrays.java
@@ -0,0 +1,39 @@
+public class MakingArrays {
+    String[] food = new String [6];
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        MakingArrays make = new MakingArrays();
+
+
+
+
+    }
+
+
+
+    public MakingArrays(){
+        System.out.println("array making in progress");
+        System.out.println("some food:");
+        food[0] = "lasagna, ";
+        food[1] = "curry, ";
+        food[2] = "aranchini, ";
+        food[3] = "veggie dumplings, ";
+        food[4] = "spicy tuna handroll, and ";
+        food[5] = "miso soup.";
+        PrintArray();
+
+
+
+
+    }
+
+
+public void PrintArray(){
+    System.out.println(food[0] + food[1] + food[2] + food[3] + food[4] + food[5]);
+
+
+}
+
+
+}
