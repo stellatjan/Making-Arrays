2ed9a6b14ec1fa5644fd758c982ff6c471056aeb
stellatjan <stella_tjan27@milton.edu>
2025-02-26 15:40:37 -0500

    hw

--- src/ArraysPhotos.java
+++ src/ArraysPhotos.java
@@ -10,9 +10,50 @@ Sushi[] Sushi = new Sushi [10];
     //constructor
     public ArraysPhotos(){
         System.out.println("making arrays with photos!");
+
         Sushi spicytunahandroll = new Sushi("handroll");
         Sushi[0] = spicytunahandroll;
         Sushi[0].displaySushiInfo();
+
+        Sushi sakesashimi = new Sushi("sashimi");
+        Sushi[1] = sakesashimi;
+        Sushi[1].displaySushiInfo();
+
+        Sushi salmonavocadoroll = new Sushi("makimono");
+        Sushi[2] = salmonavocadoroll;
+        Sushi[2].displaySushiInfo();
+
+        Sushi unagi = new Sushi("nigiri");
+        Sushi[3] = unagi;
+        Sushi[3].displaySushiInfo();
+
+        Sushi misosoup = new Sushi("soup, appetizer");
+        Sushi[4] = misosoup;
+        Sushi[4].displaySushiInfo();
+
+        Sushi shishitopeppers = new Sushi("small dish, appetizer");
+        Sushi[5] = shishitopeppers;
+        Sushi[5].displaySushiInfo();
+
+        Sushi salmonnigiri = new Sushi("nigiri");
+        Sushi[6] = salmonnigiri;
+        Sushi[6].displaySushiInfo();
+
+        Sushi izakayatuna = new Sushi("crispy rice with tuna");
+        Sushi[7] = izakayatuna;
+        Sushi[7].displaySushiInfo();
+
+        Sushi hamachikama = new Sushi("girlled yellowtail with lemon, main dish");
+        Sushi[8]= hamachikama;
+        Sushi[8].displaySushiInfo();
+
+        Sushi chefschoice = new Sushi("Chef's choice of sashimi and nigiri");
+        Sushi[9]=chefschoice;
+        Sushi[9].displaySushiInfo();
+
+
+
+
     }
 }
 
--- src/Sushi.java
+++ src/Sushi.java
@@ -7,7 +7,7 @@ public class Sushi {
     int numberOfPieces;
     boolean containsSoy;
 
-    // Constructor
+    // Constructor for Pstyle
     public Sushi(String Pstyle) {
         name = "Spicy tuna handroll";
         style = Pstyle;
@@ -17,13 +17,64 @@ public class Sushi {
         displaySushiInfo();
     }
 
+    //i dont know how to impletment these and the name does not work
+
+//    // Constructor for name only
+//    public Sushi(String Pname) {
+//        name = Pname;
+//        style = "Handroll";  // default style
+//        price = 12.99;  // default price
+//        numberOfPieces = 8;  // default pieces
+//        containsSoy = true;  // default soy
+//        displaySushiInfo();
+//    }
+//
+//    // Constructor for # pieces only
+//    public Sushi(int PnumberOfPieces) {
+//        name = "Spicy tuna handroll";  // default name
+//        style = "Handroll";  // default style
+//        price = 12.99;  // default price
+//        numberOfPieces = PnumberOfPieces;
+//        containsSoy = true;  // default soy
+//        displaySushiInfo();
+//    }
+//
+//
+//    //constructor for price only
+//    public Sushi(double Pprice) {
+//        name = "Spicy tuna handroll";  // default name
+//        style = "Handroll";  // default style
+//        price = Pprice;
+//        numberOfPieces = 8;  // default pieces
+//        containsSoy = true;  // default soy
+//        displaySushiInfo();
+//    }
+//
+//    // Constructor for containsSoy only
+//    public Sushi(boolean PcontainsSoy) {
+//        name = "Spicy tuna handroll";  // default name
+//        style = "Handroll";  // default style
+//        price = 12.99;  // default price
+//        numberOfPieces = 8;  // default pieces
+//        containsSoy = PcontainsSoy;
+//        displaySushiInfo();
+//    }
+
+
+
+
 //To display the information of the sushi
     public void displaySushiInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Style: " + style);
-        System.out.println("Price: $" + price);
-        System.out.println("Number of pieces: " + numberOfPieces);
-        System.out.println("Contains soy: " + containsSoy);
+        System.out.println("Name: " + name + "" +
+                "");
+        System.out.println("Style: " + style + "" +
+                "");
+        System.out.println("Price: $" + price + "" +
+                "");
+        System.out.println("Number of pieces: " + numberOfPieces + "" +
+                "");
+        System.out.println("Contains soy: " + containsSoy + "" +
+                "");
     }
 
 }
